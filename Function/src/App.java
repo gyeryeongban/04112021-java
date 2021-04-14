a957db94f71b25c761878fe8f6db25127f9edb82
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-04-14 18:02:01 +0900

    Step 1

---
+++ Function/src/App.java
@@ -0,0 +1,12 @@
+// 정수 N개의 합
+public class App {
+    long sum(int[] a);
+
+    long answer = 0;
+
+    for(
+    int i = 0;i<answer.lenght;i++)
+    {
+        answer += a[i];
+    }return answer;
+}
